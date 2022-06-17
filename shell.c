41b319f15eec847fa47cd7220f88b6cc090b5bc1
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-06-17 23:37:45 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,22 +1,18 @@
 #include "main.h"
 int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
 {
-
-		int int_mode = 1;
-			int errcode = 0;
-
-
-				errcode = errcode + 0;
-
-					int_mode = isatty(STDIN_FILENO);
-						if (int_mode == 0)
-								{
-											notty(av);
-												}
-							else
-									{
-
-												_interactive(av);
-													}
-								return (0);
+	int int_mode = 1;
+	int errcode = 0;
+	
+	errcode = errcode + 0;
+	int_mode = isatty(STDIN_FILENO);
+	if (int_mode == 0)
+	{
+		notty(av);
+	}
+	else
+	{
+		_interactive(av);
+	}
+	return (0);
 }
